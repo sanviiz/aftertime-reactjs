bd285ea4cc6b45a03044d21a242261682cc18c33
test <sanviiz06@hotmail.com>
2021-02-27 16:35:53 +0700

    Fixed Cache Images - Add More Images

--- src/components/CacheImages.js
+++ src/components/CacheImages.js
@@ -1,5 +1,7 @@
 import { QuestionBg } from "./QuestionBg";
 import IntroBg00 from "../assets/img/intro/intro-bg.png";
 import IntroBg01 from "../assets/img/intro/intro-element.png";
+import YesBg from "../assets/img/summary/yes-bg.png";
+import NoBg from "../assets/img/summary/no-bg.png";
 
-export const CacheImages = [...QuestionBg, IntroBg00, IntroBg01];
+export const CacheImages = [...QuestionBg, IntroBg00, IntroBg01, YesBg, NoBg];
