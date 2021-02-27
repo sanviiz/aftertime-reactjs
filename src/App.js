0d26d0d18bc49e110c6e4839910c03b3ef2486e8
test <sanviiz06@hotmail.com>
2021-02-27 14:01:00 +0700

    Add Cache Images Function and Loading Screen

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Routes from "./Routes";
 import "./App.css";
+import { css } from "@emotion/core";
+import ClockLoader from "react-spinners/ClockLoader";
+import { CacheImages } from "./components/CacheImages";
 
 function App() {
+  const override = css`
+    display: block;
+    margin: 0 auto;
+  `;
+
+  const color = "#fff";
+
+  const [loading, setLoading] = useState(true);
+
+  const cacheImages = async (srcArray) => {
+    const promises = await srcArray.map((src) => {
+      return new Promise(function (resolve, reject) {
+        const img = new Image();
+        img.src = src;
+        img.onload = resolve();
+        img.onerror = reject();
+      });
+    });
+    await Promise.all(promises);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    cacheImages(CacheImages);
+  }, []);
+
   return (
     <div className="App">
-      <Routes />
+      {loading ? (
+        <div className="loading-container">
+          <ClockLoader color={color} css={override} size={100} />
+        </div>
+      ) : (
+        <Routes />
+      )}
     </div>
   );
 }
---
+++ src/components/CacheImages.js
@@ -0,0 +1,5 @@
+import { QuestionBg } from "./QuestionBg";
+import IntroBg00 from "../assets/img/intro/intro-bg.png";
+import IntroBg01 from "../assets/img/intro/intro-element.png";
+
+export const CacheImages = [...QuestionBg, IntroBg00, IntroBg01];
